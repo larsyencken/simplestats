2c7f236c8e30c65300eb5121f9e5bbedae24dcd9
Lars Yencken <lljy@csse.unimelb.edu.au>
2009-05-01 16:31:59 +1000

    Removes dependency on cjktools, and increments version to 0.1.1.

--- src/freq.py
+++ src/freq.py
@@ -11,9 +11,12 @@
 
 #----------------------------------------------------------------------------#
 
-from math import log
+import sys
+import bz2
+import gzip
+import codecs
 
-from cjktools.common import sopen
+from math import log
 
 #----------------------------------------------------------------------------#
 
@@ -427,3 +430,38 @@ def _contains_escape(value):
     True
     """
     return _space_replacement in value
+
+def sopen(filename, mode='rb', encoding='utf8'):
+    """
+    Transparently uses compression on the given file based on file
+    extension.
+
+    @param filename: The filename to use for the file handle.
+    @param mode: The mode to open the file in, e.g. 'r' for read, 'w' for
+        write, 'a' for append.
+    @param encoding: The encoding to use. Can be set to None to avoid
+        using unicode at all.
+    """
+    read_mode = 'r' in mode
+    if read_mode and 'w' in mode:
+        raise Exception, "Must be either read mode or write, but not both"
+
+    if filename.endswith('.bz2'):
+        stream = bz2.BZ2File(filename, mode)
+    elif filename.endswith('.gz'):
+        stream = gzip.GzipFile(filename, mode)
+    elif filename == '-':
+        if read_mode:
+            stream = sys.stdin
+        else:
+            stream = sys.stdout
+    else:
+        stream = open(filename, mode)
+
+    if encoding not in (None, 'byte'):
+        if read_mode:
+            return codecs.getreader(encoding)(stream)
+        else:
+            return codecs.getwriter(encoding)(stream)
+
+    return stream
