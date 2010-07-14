b4afdaf835ffb23ae4171aa86e6445b2d102f0cb
Lars Yencken <lljy@csse.unimelb.edu.au>
2010-07-14 17:44:11 +1000

    Reverts previous change and adds warnings instead.
    Now, when used with Python 2.6 and later, warnings will be emitted which
    suggest better replacements.

--- src/comb.py
+++ src/comb.py
@@ -11,17 +11,54 @@
 Combinations and permuatations of items.
 """
 
-import itertools
+from itertools import ifilter
+import warnings
+import sys
 
+class deprecated(object):
+    def __init__(self, py_version, module_and_method):
+        self.py_version = py_version
+        self.module_and_method = module_and_method
+
+    def __call__(self, f):
+        def g(*args, **kwargs):
+            if sys.version_info >= self.py_version:
+                warnings.warn(
+                    "method %s() can be replaced by %s() in Python "
+                    "%d.%d and above" % (f.__name__, self.module_and_method,
+                    self.py_version[0], self.py_version[1])
+                )
+            return f(*args, **kwargs)
+        g.__name__ = f.__name__
+        g.__doc__ = f.__doc__
+
+        return g
+
+@deprecated((2, 6), 'itertools.product')
 def combinations(*combination_list):
     """
     Generates a list of all possible combinations of one element from the
     first item in combination_list, one from the second, etc. For example::
 
         >>> combinations([1, 2], ['dog'], ['a', 'b'])
-        [(1, 'dog', 'a'), (1, 'dog', 'b'), (2, 'dog', 'a'), (2, 'dog', 'b')]
+        [(1, 'dog', 'a'), (2, 'dog', 'a'), (1, 'dog', 'b'), (2, 'dog', 'b')]
     """
-    return list(itertools.product(*combination_list))
+    combination_list = list(combination_list[:])
+    combination_list.reverse()
+
+    first_list = combination_list.pop()
+    combos = map(lambda x: (x,), first_list)
+
+    while combination_list:
+        next_level_combos = []
+        for item_to_add in combination_list.pop():
+            # add this item to the end of every existing combo 
+            for existing_combo in combos:
+                next_level_combos.append(existing_combo + (item_to_add,))
+
+        combos = next_level_combos
+
+    return combos
 
 #----------------------------------------------------------------------------#
 
@@ -39,44 +76,81 @@ class UniquePairsIterator(object):
     [(1, 2), (1, 3), (2, 3)]
     """
     def __init__(self, input_list):
-        self.input_list = input_list
-        list_len = len(input_list)
-        if list_len == 0:
-            self.size = 0
-        elif list_len == 1:
-            self.size = 1
-        else:
-            self.size = list_len * (list_len - 1) / 2
+        self.i = 0
+        self.j = 1
+        self.input_list = sorted(input_list)
+        self.list_len = len(input_list)
 
-    def __iter__(self):
-        for v in itertools.combinations(self.input_list, 2):
-            yield v
+        if self.list_len < 2:
+            raise ValueError, "input must be of length at least 2"
+
+    def next(self):
+        if self.i == self.list_len - 1 and self.j >= self.list_len:
+            raise StopIteration
+
+        item = self.input_list[self.i], self.input_list[self.j]
+        self.j += 1
+        if self.j >= self.list_len:
+            self.i += 1
+            self.j = self.i + 1
+
+        return item
 
     def __len__(self):
-        return self.size
+        return self.list_len * (self.list_len - 1) / 2
+
+    def __iter__(self):
+        return self
 
     def __repr__(self):
         return '<UniquePairsIterator: %d items>' % len(self)
 
 #----------------------------------------------------------------------------#
 
+@deprecated((2, 6), 'itertools.combinations')
 def unique_tuples(input_list, n=2):
     "Similar to combinations, but selects from the same list."
-    return list(itertools.combinations(input_list, n))
+    def filter_fn(x):
+        for i in xrange(n-1):
+            if x[i] >= x[i+1]:
+                return False
+        else:
+            return True
+
+    return filter(filter_fn, combinations(*(n*[input_list])))
 
 #----------------------------------------------------------------------------#
 
+@deprecated((2, 6), 'itertools.combinations')
 def iunique_tuples(input_list, n=2):
     "An iterator version of unique_tuples."
-    return itertools.combinations(input_list, n)
+    def filter_fn(x):
+        for i in xrange(n-1):
+            if x[i] >= x[i+1]:
+                return False
+        else:
+            return True
+
+    return ifilter(filter_fn, icombinations(*(n*[input_list])))
 
 #----------------------------------------------------------------------------#
 
+@deprecated((2, 6), 'itertools.product')
 def icombinations(*combination_lists):
     """
     As for combinations(), but returns an iterator.
     """
-    return itertools.product(*combination_lists)
+    combination_lists = map(list, combination_lists)
+    lengths = map(len, combination_lists)
+    combined = zip(combination_lists, lengths)
+    n_combs = reduce(lambda x, y: x*y, lengths)
+
+    for i in xrange(n_combs):
+        item = ()
+        for item_list, list_length in combined:
+            i, offset = divmod(i, list_length)
+            item += (item_list[offset],)
+        yield item
 
 #----------------------------------------------------------------------------#
 
@@ -86,7 +160,7 @@ def combination_seqs(*combination_list):
     assumed to already be in sequence form. For example::
 
         >>> combination_seqs([(1, 2), (3, 4)], [('dog',), ('cat',)])
-        [(1, 2, 'dog'), (1, 2, 'cat'), (3, 4, 'dog'), (3, 4, 'cat')]
+        [(1, 2, 'dog'), (3, 4, 'dog'), (1, 2, 'cat'), (3, 4, 'cat')]
     """
     return list(icombination_seqs(*combination_list))
 
@@ -97,14 +171,13 @@ def icombination_seqs(*combination_lists):
     As for combinations(), but returns an iterator.
 
         >>> list(icombination_seqs([(1, 2), (3, 4)], [('dog',), ('cat',)]))
-        [(1, 2, 'dog'), (1, 2, 'cat'), (3, 4, 'dog'), (3, 4, 'cat')]
+        [(1, 2, 'dog'), (3, 4, 'dog'), (1, 2, 'cat'), (3, 4, 'cat')]
     """
     for seq_combs in icombinations(*combination_lists):
         result = []
         for seq in seq_combs:
             result.extend(seq)
         yield tuple(result)
-    return
 
 #----------------------------------------------------------------------------#
 
