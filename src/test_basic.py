e471e80c8dd5c2620b679e32e4a827365c96a9fc
Lars Yencken <lars@yencken.org>
2011-11-03 17:00:58 +1100

    Prune repeated comb module tests.

--- src/test_basic.py
+++ src/test_basic.py
@@ -7,7 +7,7 @@
 #  Copyright 2009 Lars Yencken. All rights reserved.
 #
 
-import sys, unittest
+import unittest
 import doctest
 
 import basic
@@ -84,46 +84,6 @@ class BasicStatsTest(unittest.TestCase):
 
     def tearDown(self):
         pass
-
-#----------------------------------------------------------------------------#
-
-
-#----------------------------------------------------------------------------#
-
-class CombinationTest(unittest.TestCase):
-    def setUp(self):
-        self.dataA = [1,2,3]
-
-    def testCombinations(self):
-        self.assertEqual(combinations([self.dataA, self.dataA]),
-                [(1,1),(2,1),(3,1),(1,2),(2,2),(3,2),(1,3),(2,3),(3,3)])
-        return
-
-    def testICombinations(self):
-        self.assertEqual(list(icombinations([self.dataA, self.dataA])),
-                [(1,1),(2,1),(3,1),(1,2),(2,2),(3,2),(1,3),(2,3),(3,3)])
-
-        return
-
-    def testUniqueTuples(self):
-        self.assertEqual(unique_tuples([1,2]), [(1,2)])
-        self.assertEqual(unique_tuples([1,2,3]), [(1,2), (1,3), (2,3)])
-
-        return
-
-    def testInclusionCombination(self):
-        self.assertEqual(
-                set(map(tuple, inclusion_combinations([1,2]))),
-                set(map(tuple, [[], [1], [2], [1,2]])),
-            )
-        self.assertEqual(inclusion_combinations([]), [[]])
-        return
-
-    def testIUniqueTuples(self):
-        self.assertEqual(list(iunique_tuples([1,2])), [(1,2)])
-        self.assertEqual(list(iunique_tuples([1,2,3])), [(1,2), (1,3), (2,3)])
-
-        return
 
 #----------------------------------------------------------------------------#
 
